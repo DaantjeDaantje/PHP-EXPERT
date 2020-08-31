4b962c13741eee4095872c0a55ae77ffeea98cad
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-08-31 14:30:37 +0200

    Arrays 4

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -1,39 +1,54 @@
-<?php
+<!DOCTYPE html>
 
-$speelgoed = [
+<html>
 
-    [
-        'naam' => 'bal',
-        'soort' => 'sport',
-        'prijs' => '5.00'
-    ],
-    [
-        'naam' => 'knuffelbeer',
-        'soort' => 'knuffels',
-        'prijs' => '9.50'
-    ],
-    [
-        'naam' => 'Catan',
-        'soort' => 'bordspel',
-        'prijs' => '39.00'
-    ],
-];
+<body>
+	<?php
+        $speelgoed = [
 
+            [
+                'naam' => 'bal',
+                'soort' => 'sport',
+                'prijs' => '5.00'
+            ],
+            [
+                'naam' => 'knuffelbeer',
+                'soort' => 'knuffels',
+                'prijs' => '9.50'
+            ],
+            [
+                'naam' => 'Catan',
+                'soort' => 'bordspel',
+                'prijs' => '39.00'
+            ],
+        ];
+        
+        echo "Speelgoed " . $speelgoed[0]["naam"] . " uit de categorie " . $speelgoed[0]["soort"] . " kost " . $speelgoed[0]["prijs"] . "<br>";
+        echo "Speelgoed " . $speelgoed[1]["naam"] . " uit de categorie " . $speelgoed[1]["soort"] . " kost " . $speelgoed[1]["prijs"] . "<br>";
+        echo "Speelgoed " . $speelgoed[2]["naam"] . " uit de categorie " . $speelgoed[2]["soort"] . " kost " . $speelgoed[2]["prijs"] . "<br>";
+        
+        $games = [
+            'sport' => [
+                'naam' => 'Fifa',
+                'uitgever' => 'EA',
+                'prijs' => '60.00'
+            ],
+            'action' => [
+                'naam' => 'Doom',
+                'uitgever' => 'Bethesda',
+                'prijs' => '60.00'
+            ],
+            'fps' => [
+                'naam' => 'Left for dead 2',
+                'uitgever' => 'Valve',
+                'prijs' => '40.00'
+            ]
+        ];
 
-$games = [
-    'sport' => [
-        'naam' => 'Fifa',
-        'uitgever' => 'EA',
-        'prijs' => '60.00'
-    ],
-    'action' => [
-        'naam' => 'Doom',
-        'uitgever' => 'Bethesda',
-        'prijs' => '60.00'
-    ],
-    'fps' => [
-        'naam' => 'Left for dead 2',
-        'uitgever' => 'Valve',
-        'prijs' => '40.00'
-    ]
-];
+        echo "Speelgoed " . $games['sport']['naam'] . " uit de categorie " . $games['sport']['uitgever'] . " kost " . $games['sport']['prijs'] . "<br>";
+        echo "Speelgoed " . $games['action']['naam'] . " uit de categorie " . $games['action']['uitgever'] . "kost " . $games['action']['prijs'] . "<br>";
+        echo "Speelgoed " . $games['fps']['naam'] . " uit de categorie " . $games['fps']['uitgever'] ." kost " . $games['fps']['prijs'] . "<br>";
+	?>
+</body>
+
+</html>
