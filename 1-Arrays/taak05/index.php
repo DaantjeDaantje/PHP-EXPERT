a6a9049a1204bd5c8db9884b8ada7d8b89eaa767
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-08-31 14:37:32 +0200

    Arrays 5

---
+++ 1-Arrays/taak05/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+    $medewerkers = [
+        [
+          'voornaam' => 'Willem',
+          'achternaam' => 'van Oranje',
+          'functie'  => 'koning'
+        ],
+        [
+          'voornaam' => 'Donald',
+          'achternaam' => 'Trump',
+          'functie'  => 'president'
+        ],
+        [
+          'voornaam' => 'Homer',
+          'achternaam' => 'Simpson',
+          'functie'  => 'fabrieksarbeider'
+        ],
+        [
+          'voornaam' => 'Johan',
+          'achternaam' => 'Cruyff',
+          'functie'  => 'voetbalicoon'
+        ],
+        [
+          'voornaam' => 'Badr',
+          'achternaam' => 'Hari',
+          'functie'  => 'Kickboxer'
+        ]
+    ];
+      
+    foreach($medewerkers as $x){
+        echo $x['voornaam'] . " " . $x['achternaam'] . " is een " . $x['functie'] . "</br>";
+    }
+	?>
+</body>
+
+</html>
+ 
