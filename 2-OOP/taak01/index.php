ed6a1d5d24bb4f448cef97b40a639e484bfcca8f
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-09-01 12:19:27 +0200

    OOP 1

---
+++ 2-OOP/taak01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        class auto{
+            public $merk;
+            public $Kleur;
+            public $type;
+            public $uitvoering;
+            public $brandstof;
+        }
+
+        $nieuweAuto = new auto();
+        $nogEenAndereAuto = new auto();
+	?>
+</body>
+
+</html>
