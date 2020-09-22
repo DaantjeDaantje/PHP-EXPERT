e8aa8b71a9c1083188e1bd11ca808fba79f82101
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-09-22 22:36:14 +0200

    PDO SELECT 2

---
+++ 3-PDO-SELECT/taak02/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php		
+	$database_lokatie     = 'localhost';
+	$database_naam        = 'ja';
+	$database_gebruiker   = 'root';
+	$database_wachtwoord  = '';
+	$database_connectie = new PDO("mysql:host=localhost;dbname=ja", "root", "");
+
+	$sql = "SELECT * FROM locations";
+	$statement = $database_connectie->prepare($sql); 
+	$statement->execute();
+	$database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	foreach($database_gegevens as $gebruiker){  
+	  echo $gebruiker['name'] . "</br> Adres: " . $gebruiker['address'] . "</br> stad: " . $gebruiker['city'] . "</br></br>";
+	}
+	?>
+</body>
+
+</html>
